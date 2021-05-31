6be03cdc8ef0a167b86468238b7e2f7f16363903
jason-ng-zq99 <54024886+jason-ng-zq99@users.noreply.github.com>
2021-05-31 17:39:03 +0800

    Edited psql.txt to smoothen setup process

--- server/config/config.go
+++ server/config/config.go
@@ -2,10 +2,10 @@ package config
 
 const (
 	DB_HOST     = "localhost"
-	DB_PORT     = 5433
+	DB_PORT     = 5432
 	DB_USER     = "postgres"
-	DB_PASSWORD = "password"
-	DB_NAME     = "testing1"
+	DB_PASSWORD = "root"
+	DB_NAME     = "goauth"
 
 	LOCAL_HOST = "localhost:3002"
 )

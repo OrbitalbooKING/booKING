c120b406542ebbde89b4cc71263905d33a559545
Jie Wei <77203354+jiewei98@users.noreply.github.com>
2021-06-21 22:23:21 +0800

    Edited frontend files in case server is not up

--- client/src/components/Reset.js
+++ client/src/components/Reset.js
@@ -102,9 +102,22 @@ function ResetForm() {
             }).then((response) => {
                 history.push("/reset-password-success"); 
             }).catch((error) => {
-                if (error.response.status === 400) {
-                    setError(error.response.data.message);
+                if (error.response) {
+                    console.log("response");
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    if (error.response.status === 400) {
+                        setError(error.response.data.message);
+                    }
+                } else if (error.request) {
+                    console.log("request");
+                    // The request was made but no response was received
+                    // `error.request` is an instance of XMLHttpRequest in the 
+                    // browser and an instance of
+                    // http.ClientRequest in node.js
+                    console.log(error.request);
                 } else {
+                    // Something happened in setting up the request that triggered an Error
                     setError("Query failed!");
                 }
             });

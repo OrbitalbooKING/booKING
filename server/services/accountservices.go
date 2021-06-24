1baf196fa2ad4637c781aefaae627c04d6ada0a3
jason-ng-zq99 <54024886+jason-ng-zq99@users.noreply.github.com>
2021-06-24 23:09:10 +0800

    Refactored some code
    Also added response message for resetPassword

--- server/services/accountservices.go
+++ server/services/accountservices.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"fmt"
+	"github.com/jinzhu/gorm"
 	"log"
 	"net/http"
 	"server/models"
@@ -23,30 +24,34 @@ func Register(c *gin.Context) {
 	}
 
 	// check if account already exists
-	var retrieved models.Accounts
-	if DB.Where("nusnetid = ?", input.Nusnetid).First(&retrieved).RowsAffected != 0 {
+	if GetAccountExists(DB, input) {
 		c.JSON(http.StatusBadRequest, gin.H{"success": false, "message": "Account already exists!"})
 		fmt.Println("Account already exists.")
-		return
 	}
 
 	// hashing the password
-	input.Password = utils.CreateHashedPassword(input.Password)
+	user := models.User{
+		Nusnetid: input.Nusnetid,
+		Password: input.Password,
+	}
+	err := utils.HashPassword(&user)
+	if err != nil {
+		fmt.Println("Error in hashing user password. " + err.Error())
+	}
+	input.Password = user.Password
 
 	// get accountTypeID
-	var accountType models.Accounttypes
-	if err := DB.Where("accounttypename = ?", "Student").First(&accountType).Error; err != nil {
+	accountType, err := GetAccountTypeDetails(DB, "Student")
+	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"success": false, "message": "Unable to determine account type"})
 		fmt.Println("Unable to get accountTypeID. " + err.Error() + "\n")
-		return
 	}
 
 	// get accountStatusID
-	var accountStatus models.Accountstatuses
-	if err := DB.Where("accountstatusname = ?", "Offline").First(&accountStatus).Error; err != nil {
+	accountStatus, err := GetAccountStatus(DB, "Offline")
+	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"success": false, "message": "Unable to determine account status"})
 		fmt.Println("Unable to get accountStatusID. " + err.Error() + "\n")
-		return
 	}
 
 	account := models.Accounts{
@@ -62,10 +67,10 @@ func Register(c *gin.Context) {
 		Lastupdated:     time.Now(),
 		Accountstatusid: accountStatus.ID,
 	}
-	if err := DB.Create(&account).Error; err != nil {
+
+	if err := CreateAccount(DB, account); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"success": false, "error": "Unable to create account"})
 		fmt.Println("Unable to crete account. " + err.Error() + "\n")
-		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{"success": true, "message": "Account successfully created!"})
@@ -75,11 +80,10 @@ func Register(c *gin.Context) {
 // GET /get_faculty
 // get a list of faculties to populate registration field
 func GetFaculty(c *gin.Context) {
-	var facultyList []models.Faculties
-	if err := DB.Find(&facultyList).Error; err != nil {
+	facultyList, err := GetFacultyList(DB)
+	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"success": false, "message": "Unable to get list of faculties."})
 		fmt.Println("Error getting list of faculties. " + err.Error() + "\n")
-		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{"data": facultyList})
@@ -95,44 +99,39 @@ func Login(c *gin.Context) {
 		return
 	}
 
-	var retrieved models.Accounts
-	row := DB.Where("nusnetid = ?", input.Nusnetid).First(&retrieved)
-	if row.Error != nil {
+	retrieved, err := GetAccount(DB, input)
+	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"success": false, "message": "Invalid NUSNET ID!"})
-		fmt.Println("Invalid NUSNET ID input. " + row.Error.Error() + "\n")
-		return
+		fmt.Println("Invalid NUSNET ID input. " + err.Error() + "\n")
 	}
 
-	match := utils.CheckPasswordHash(input.Password, retrieved.Passwordhash) // non hashed input first, followed by hashed one retrieved from DB
-
+	match := utils.CheckPasswordHash(input, retrieved.Passwordhash) // non hashed input first, followed by hashed one retrieved from DB
 	if !match {
 		c.JSON(http.StatusBadRequest, gin.H{"success": false, "message": "Invalid password!"})
 		fmt.Println("Invalid password.")
 		return
-	} else {
-		// get online status code
-		var statusCode models.Accountstatuses
-		if err := DB.Where("accountstatusname = ?", "Online").First(&statusCode).Error; err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Unable to retrieve online status code. "})
-			fmt.Println("Unable to retrieve online status code. " + err.Error() + "\n")
-			return
-		}
-
-		// change account status
-		updateQuery := "UPDATE accounts SET accountstatusid = ?, lastupdated = ? WHERE nusnetid = ?"
-		if err := DB.Exec(updateQuery, statusCode.ID, time.Now(), input.Nusnetid).Error; err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Unable to update account logged in status. " + err.Error() + "\n"})
-			fmt.Println("Unable to update account logged in status. " + err.Error() + "\n")
-			return
-		}
-
-		user := models.LoginOutput{
-			Nusnetid: input.Nusnetid,
-			Name:     retrieved.Name,
-		}
-		c.JSON(http.StatusOK, gin.H{"success": true, "message": user})
-		fmt.Println("Log in successful!")
 	}
+
+	// get online status code
+	statusCode, err := GetAccountStatus(DB, "Online")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Unable to retrieve online status code. "})
+		fmt.Println("Unable to retrieve online status code. " + err.Error() + "\n")
+		return
+	}
+
+	// change account status
+	if err := UpdateAccountStatus(DB, statusCode, input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Unable to update account logged in status. " + err.Error() + "\n"})
+		fmt.Println("Unable to update account logged in status. " + err.Error() + "\n")
+	}
+
+	user := models.LoginOutput{
+		Nusnetid: input.Nusnetid,
+		Name:     retrieved.Name,
+	}
+	c.JSON(http.StatusOK, gin.H{"success": true, "message": user})
+	fmt.Println("Log in successful!")
 }
 
 // PATCH /reset
@@ -142,20 +141,31 @@ func ResetPassword(c *gin.Context) {
 	// Validate input
 	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"success": false, "message": "Have you input correctly? " + err.Error()})
-		fmt.Println("Error parsing reset inputs. " + err.Error() + "\n")
+		fmt.Println("Error parsing reset inputs. " + err.Error())
 		return
 	}
 
 	// check if account already exists
-	var retrieved models.Accounts
-	if DB.Where("nusnetid = ?", input.Nusnetid).First(&retrieved).RowsAffected == 0 {
+	if !GetAccountExists(DB, input) {
 		c.JSON(http.StatusBadRequest, gin.H{"success": false, "message": "Account does not exist!"})
 		fmt.Println("Unable to find account.")
 		return
 	}
 
+	// get account
+	user := models.User{
+		Nusnetid: input.Nusnetid,
+		Password: input.Password,
+	}
+	retrieved, err := GetAccount(DB, user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"success": false, "message": "Unable to get account!" + err.Error()})
+		fmt.Println("Unable to get account." + err.Error())
+		return
+	}
+
 	// check if new password is same as the old password
-	match := utils.CheckPasswordHash(input.Password, retrieved.Passwordhash) // non hashed input first, followed by hashed one retrieved from DB
+	match := utils.CheckPasswordHash(user, retrieved.Passwordhash) // non hashed input first, followed by hashed one retrieved from DB
 	if match {
 		c.JSON(http.StatusBadRequest, gin.H{"success": false, "message": "New password cannot be the same as old password!"})
 		fmt.Println("Old password reused.")
@@ -163,29 +173,19 @@ func ResetPassword(c *gin.Context) {
 	}
 
 	// hashing the password
-	input.Password = utils.CreateHashedPassword(input.Password)
+	if err := utils.HashPassword(&user); err != nil {
+		fmt.Println("Error in hashing user password: " + err.Error())
+	}
+	input.Password = user.Password
 
-	if err := DB.Model(&retrieved).UpdateColumn("password", input.Password).Error; err != nil {
+	// update password
+	if err := UpdateAccountPassword(DB, retrieved, input); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "Unable to reset password."})
 		fmt.Println("Error in updating database. " + err.Error() + "\n")
-		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"success": true})
+	c.JSON(http.StatusOK, gin.H{"success": true, "message": "Account successfully reset!"})
 	log.Println("Password successfully reset!")
-}
-
-// GET /accounts
-// get accounts, only shows the ID
-func GetAccounts(c *gin.Context) {
-	var accounts []models.Accounts
-	if err := DB.Select("id", "nusnetid").Find(&accounts).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"data": accounts})
-	fmt.Println("Return successful!")
 }
 
 // GET /get_profile
@@ -198,8 +198,9 @@ func GetProfile(c *gin.Context) {
 		return
 	}
 
-	var account models.Accounts
-	if err := DB.Find(&account).Where("nusnetid = ?", user.Nusnetid).Scan(&account).Error; err != nil {
+	// get account from user input
+	account, err := GetAccount(DB, user)
+	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "message": "Check database query"})
 		fmt.Println("Error in retrieving profile details from Database. " + err.Error() + "\n")
 		return
@@ -219,32 +220,84 @@ func GetBookings(c *gin.Context) {
 		return
 	}
 
-	var booking []models.Currentbookings
-	if err := DB.Find(&booking).Where("nusnetid = ?", user.Nusnetid).Scan(&booking).Error; err != nil {
+	booking, err := RetrieveUserBookings(DB, user)
+	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "message": "Check database query"})
 		fmt.Println("Error in retrieving booking details from Database. " + err.Error() + "\n")
-		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{"data": booking})
 	fmt.Println("Successfully retrieved booking details.")
 }
 
-// DELETE /accounts/:id
-// Delete an account
-func DeleteAccount(c *gin.Context) {
-	// Get model if exist
-	var account models.Accounts
-	if err := DB.Where("nusnetid = ?", c.Param("nusnetid")).First(&account).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
-		return
-	}
 
-	if err := DB.Delete(&account).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
+func GetAccountTypeDetails(DB *gorm.DB, theType string) (models.Accounttypes, error) {
+	var accountType models.Accounttypes
+	query := "SELECT * FROM accounttypes WHERE accounttypename = ?"
+	if err := DB.Raw(query, theType).Scan(&accountType).Error; err != nil {
+		return models.Accounttypes{}, err
 	}
+	return accountType, nil
+}
 
-	c.JSON(http.StatusOK, gin.H{"data": true})
-	log.Println("Account successfully deleted!")
+func GetAccountExists(DB *gorm.DB, input models.CreateAccountInput) bool {
+	var retrieved models.Accounts
+	return DB.Where("nusnetid = ?", input.Nusnetid).First(&retrieved).RowsAffected != 0
+}
+
+// get accountStatusID
+func GetAccountStatus(DB *gorm.DB, statusName string) (models.Accountstatuses, error) {
+	var accountStatus models.Accountstatuses
+	if err := DB.Where("accountstatusname = ?", statusName).First(&accountStatus).Error; err != nil {
+		return models.Accountstatuses{}, err
+	}
+	return accountStatus, nil
+}
+
+// create account
+func CreateAccount(DB *gorm.DB, account models.Accounts) error {
+	if err := DB.Create(&account).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetFacultyList(DB *gorm.DB) ([]models.Faculties, error) {
+	var facultyList []models.Faculties
+	if err := DB.Find(&facultyList).Error; err != nil {
+		return nil, err
+	}
+	return facultyList, nil
+}
+
+func GetAccount(DB *gorm.DB, input models.User) (models.Accounts, error) {
+	var retrieved models.Accounts
+	row := DB.Where("nusnetid = ?", input.Nusnetid).First(&retrieved)
+	if row.Error != nil {
+		return models.Accounts{}, row.Error
+	}
+	return retrieved, nil
+}
+
+func UpdateAccountStatus(DB *gorm.DB, statusCode models.Accountstatuses, input models.User) error {
+	updateQuery := "UPDATE accounts SET accountstatusid = ?, lastupdated = ? WHERE nusnetid = ?"
+	if err := DB.Exec(updateQuery, statusCode.ID, time.Now(), input.Nusnetid).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func UpdateAccountPassword(DB *gorm.DB, retrieved models.Accounts, input models.CreateAccountInput) error {
+	if err := DB.Model(&retrieved).UpdateColumn("password", input.Password).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func RetrieveUserBookings(DB *gorm.DB, user models.User) ([]models.Currentbookings, error) {
+	var booking []models.Currentbookings
+	if err := DB.Find(&booking).Where("nusnetid = ?", user.Nusnetid).Scan(&booking).Error; err != nil {
+		return nil, err
+	}
+	return booking, nil
 }

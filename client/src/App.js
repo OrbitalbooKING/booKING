778cd327d96258546cc00a035cdd6d7a951f1b5d
Jie Wei <77203354+jiewei98@users.noreply.github.com>
2021-07-22 21:20:11 +0800

    Added one route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Help from "./components/Help";
 import HelpLoggedIn from "./components/HelpLoggedIn";
@@ -16,6 +16,8 @@ import BookingOverview from "./components/BookingOverview";
 import BookingSuccess from "./components/BookingSuccess";
 import LogoutSuccess from "./components/LogoutSuccess";
 import Profile from "./components/Profile";
+import EditProfile from "./components/EditProfile";
+import EditProfileSuccess from "./components/EditProfileSuccess";
 
 import HelpStaff from "./components/HelpStaff";
 import StaffLoginSuccess from "./components/StaffLoginSuccess";
@@ -34,54 +36,127 @@ import EditSuccess from "./components/EditSuccess";
 import DeletionOverview from "./components/DeletionOverview";
 import DeletionSuccess from "./components/DeletionSuccess";
 
-function App() {
+import TransferPoints from "./components/TransferPoints";
+import TransferSuccess from "./components/TransferSuccess";
 
+function App() {
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path="/"><LoginForm /></Route>
-          <Route exact path="/help"><Help /></Route>
-          <Route exact path="/help-logged-in"><HelpLoggedIn /></Route>
-          <Route exact path="/sign-in"><LoginForm /></Route>
-          <Route exact path="/sign-up"><SignUpForm /></Route>
-          <Route exact path="/reset-password"><ResetForm /></Route>
-          {/* <Route exact path="/sign-in-success"><LoginSuccess /></Route> */}
-          <Route exact path="/sign-in-success" render={props=>(
-            <LoginSuccess {...props} />)}
+          <Route exact path="/">
+            <LoginForm />
+          </Route>
+          <Route exact path="/help">
+            <Help />
+          </Route>
+          <Route exact path="/help-logged-in">
+            <HelpLoggedIn />
+          </Route>
+          <Route exact path="/sign-in">
+            <LoginForm />
+          </Route>
+          <Route exact path="/sign-up">
+            <SignUpForm />
+          </Route>
+          <Route exact path="/reset-password">
+            <ResetForm />
+          </Route>
+          <Route
+            exact
+            path="/sign-in-success"
+            render={(props) => <LoginSuccess {...props} />}
           />
-          <Route exact path="/sign-up-success" render={props=>(
-            <SignUpSuccess {...props} />)}
+          <Route
+            exact
+            path="/sign-up-success"
+            render={(props) => <SignUpSuccess {...props} />}
           />
-          <Route exact path="/reset-password-success" render={props=>(
-            <ResetSuccess {...props} />)}
+          <Route
+            exact
+            path="/reset-password-success"
+            render={(props) => <ResetSuccess {...props} />}
           />
-          <Route exact path="/home"><Home /></Route>
-          <Route exact path="/booking"><Booking /></Route>
-          <Route exact path="/booking-overview"><BookingOverview /></Route>
-          <Route exact path="/booking-success"><BookingSuccess /></Route>
-          <Route exact path="/logout" render={props=>(
-            <LogoutSuccess {...props} />)}
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/booking">
+            <Booking />
+          </Route>
+          <Route exact path="/booking-overview">
+            <BookingOverview />
+          </Route>
+          <Route exact path="/booking-success">
+            <BookingSuccess />
+          </Route>
+          <Route
+            exact
+            path="/logout"
+            render={(props) => <LogoutSuccess {...props} />}
           />
-          <Route exact path="/profile"><Profile /></Route>
-          <Route exact path="/help-staff"><HelpStaff /></Route>
-          <Route exact path="/staff-sign-in-success"><StaffLoginSuccess /></Route>
-          <Route exact path="/staff-home"><StaffHome /></Route>
-          <Route exact path="/staff-venues"><StaffVenues /></Route>
-          <Route exact path="/staff-profile"><StaffProfile /></Route>
-          <Route exact path="/approval-overview"><ApprovalOverview /></Route>
-          <Route exact path="/approval-success" render={props=>(
-            <ApprovalSuccess {...props} />)}
+          <Route exact path="/profile">
+            <Profile />
+          </Route>
+          <Route exact path="/edit-profile">
+            <EditProfile />
+          </Route>
+          <Route exact path="/edit-profile-success">
+            <EditProfileSuccess />
+          </Route>
+          <Route exact path="/help-staff">
+            <HelpStaff />
+          </Route>
+          <Route exact path="/staff-sign-in-success">
+            <StaffLoginSuccess />
+          </Route>
+          <Route exact path="/staff-home">
+            <StaffHome />
+          </Route>
+          <Route exact path="/staff-venues">
+            <StaffVenues />
+          </Route>
+          <Route exact path="/staff-profile">
+            <StaffProfile />
+          </Route>
+          <Route exact path="/approval-overview">
+            <ApprovalOverview />
+          </Route>
+          <Route
+            exact
+            path="/approval-success"
+            render={(props) => <ApprovalSuccess {...props} />}
           />
-          <Route exact path="/edit-home"><EditHome /></Route>
-          <Route exact path="/edit-booking"><EditBooking /></Route>
-          <Route exact path="/edit-overview"><EditOverview /></Route>
-          <Route exact path="/edit-success"><EditSuccess /></Route>
-          <Route exact path="/deletion-overview"><DeletionOverview /></Route>
-          <Route exact path="/deletion-success" render={props=>(
-            <DeletionSuccess {...props} />)}
+          <Route exact path="/edit-home">
+            <EditHome />
+          </Route>
+          <Route exact path="/edit-booking">
+            <EditBooking />
+          </Route>
+          <Route exact path="/edit-overview">
+            <EditOverview />
+          </Route>
+          <Route exact path="/edit-success">
+            <EditSuccess />
+          </Route>
+          <Route exact path="/deletion-overview">
+            <DeletionOverview />
+          </Route>
+          <Route
+            exact
+            path="/deletion-success"
+            render={(props) => <DeletionSuccess {...props} />}
           />
-          <Route exact path="*"><ErrorPage /></Route>
+          <Route exact path="/transfer-points">
+            <TransferPoints />
+          </Route>
+          <Route
+            exact
+            path="/transfer-success"
+            render={(props) => <TransferSuccess {...props} />}
+          />
+          <Route exact path="*">
+            <ErrorPage />
+          </Route>
         </Switch>
       </div>
     </Router>

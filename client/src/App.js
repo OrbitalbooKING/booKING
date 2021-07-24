22655a892959c8b64aed7da0c70ee097c8128f31
Jie Wei <77203354+jiewei98@users.noreply.github.com>
2021-07-24 16:18:17 +0800

    Updated app routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,31 +3,29 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Help from "./components/Help";
-// import HelpLoggedIn from "./components/HelpLoggedIn";
 import LoginForm from "./components/Login";
 import LoginSuccess from "./components/LoginSuccess";
 import SignUpForm from "./components/SignUp";
 import SignUpSuccess from "./components/SignUpSuccess";
 import ResetForm from "./components/Reset";
 import ResetSuccess from "./components/ResetSuccess";
+
 import Home from "./components/Home";
 import StudentHome from "./components/StudentHome";
 import Booking from "./components/Booking";
 import BookingOverview from "./components/BookingOverview";
 import BookingSuccess from "./components/BookingSuccess";
+
 import LogoutSuccess from "./components/LogoutSuccess";
 import Profile from "./components/Profile";
 import EditProfile from "./components/EditProfile";
 import EditStaffProfile from "./components/EditStaffProfile";
 import EditProfileSuccess from "./components/EditProfileSuccess";
 
-// import HelpStaff from "./components/HelpStaff";
-// import StaffLoginSuccess from "./components/StaffLoginSuccess";
 import StaffHome from "./components/StaffHome";
 import AdminHome from "./components/AdminHome";
 import CreationSuccess from "./components/CreationSuccess";
-import StaffVenues from "./components/StaffVenues";
-// import StaffProfile from "./components/StaffProfile";
+
 import ApprovalOverview from "./components/ApprovalOverview";
 import ApprovalSuccess from "./components/ApprovalSuccess";
 import RejectionOverview from "./components/RejectionOverview";
@@ -57,39 +55,39 @@ function App() {
           <Route exact path="/help">
             <Help />
           </Route>
-          {/* <Route exact path="/help-logged-in">
-            <HelpLoggedIn />
-          </Route> */}
+
           <Route exact path="/sign-in">
             <LoginForm />
-          </Route>
-          <Route exact path="/sign-up">
-            <SignUpForm />
-          </Route>
-          <Route exact path="/reset-password">
-            <ResetForm />
           </Route>
           <Route
             exact
             path="/sign-in-success"
             render={(props) => <LoginSuccess {...props} />}
           />
+
+          <Route exact path="/sign-up">
+            <SignUpForm />
+          </Route>
           <Route
             exact
             path="/sign-up-success"
             render={(props) => <SignUpSuccess {...props} />}
           />
+
+          <Route exact path="/reset-password">
+            <ResetForm />
+          </Route>
           <Route
             exact
             path="/reset-password-success"
             render={(props) => <ResetSuccess {...props} />}
           />
 
-          <Route exact path="/student-home">
-            <StudentHome />
-          </Route>
           <Route exact path="/home">
             <Home />
+            <Route exact path="/student-home">
+              <StudentHome />
+            </Route>
           </Route>
           <Route exact path="/booking">
             <Booking />
@@ -102,6 +100,7 @@ function App() {
             path="/booking-success"
             render={(props) => <BookingSuccess {...props} />}
           />
+
           <Route
             exact
             path="/logout"
@@ -110,7 +109,6 @@ function App() {
           <Route exact path="/profile">
             <Profile />
           </Route>
-
           <Route exact path="/edit-profile">
             <EditProfile />
           </Route>
@@ -123,12 +121,6 @@ function App() {
             render={(props) => <EditProfileSuccess {...props} />}
           />
 
-          {/* <Route exact path="/help-staff">
-            <HelpStaff />
-          </Route> */}
-          {/* <Route exact path="/staff-sign-in-success">
-            <StaffLoginSuccess />
-          </Route> */}
           <Route exact path="/staff-home">
             <StaffHome />
           </Route>
@@ -140,12 +132,6 @@ function App() {
             path="/creation-success"
             render={(props) => <CreationSuccess {...props} />}
           />
-          <Route exact path="/staff-venues">
-            <StaffVenues />
-          </Route>
-          {/* <Route exact path="/staff-profile">
-            <StaffProfile />
-          </Route> */}
 
           <Route exact path="/approval-overview">
             <ApprovalOverview />
